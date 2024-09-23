527c8cba7c681ef8183f8a97a4b40638ec53b88e
Sarthak Singh <mzppsarthakjvb@gmail.com>
2024-09-24 02:15:37 +0530

    Add file changes

--- app.js
+++ app.js
@@ -87,6 +87,7 @@ app.use((req, res, next) => {
 app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
 app.use("/", userRouter);
+app.use(" ", userRouter);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
