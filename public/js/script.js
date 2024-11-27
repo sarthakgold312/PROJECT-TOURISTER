c81a1c53d2d5cbf46e40e0d792b979c2651d8b44
Sarthak Singh <mzppsarthakjvb@gmail.com>
2024-11-28 01:55:28 +0530

    Som Updates

--- public/js/script.js
+++ public/js/script.js
@@ -18,4 +18,44 @@
 })();
 
 
+// new.ejs
+function toggleOtherCategory() {
+  const categorySelect = document.getElementById('category');
+  const otherCategoryInput = document.getElementById('otherCategoryInput');
+  if(categorySelect.value === 'Other') {
+    otherCategoryInput.style.display = 'block';
+    document.getElementById('other-category').setAttribute('required', 'true');
+  }else{
+    otherCategoryInput.style.display = 'none';
+    document.getElementById('other-category').removeAttribute('required');
+  }
+}
 
+
+//index.ejs
+const filters = document.querySelectorAll('.filter');
+const listings = document.querySelectorAll('.listing-card');
+
+filters.forEach((filter)=>{
+  filter.addEventListener('click',()=>{
+    const selectedCategory = filter.getAttribute('data-category').toLowerCase();
+
+    filters.forEach((f) => {
+      f.classList.remove('active');
+    });
+
+    listings.forEach((listing) => {
+      const listingCategory = listing.getAttribute('data-category').toLowerCase();
+      if(selectedCategory === 'all' || listingCategory === selectedCategory) {
+        listing.style.display = 'block'; //show the listing
+      }else{
+        listing.style.display = 'none'; //hide listing
+      }
+    });
+    //layout after filtering
+    setTimeout(() => {
+      const row = document.querySelector('.row');
+      row.style.display = 'flex';
+    }, 100);
+  });
+});
